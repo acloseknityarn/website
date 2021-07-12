91d12799115de973544d5fd31c8f8676b7c92af0
Aryamann Verma <avverma19699@gmail.com>
2021-07-12 02:29:54 -0400

    1. Created svg social-media icons - React Component 2. Added Profile-Image and the social-media icon component on the home page

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,23 @@
 import AryamannContact from "../components/Contact";
+import SocialLinks from '../components/SocialLinks';
 import NextLink from 'next/link'
 
 export default function About(){
   return (
-    <div>
+    <div className="max-w-4xl mx-auto px-6">
       <h1 className="text-3xl mb-8">About Me</h1>
       
-      <p className="mb-5">Hey Visitor! Thanks for stopping by. I'm Aryamann which translates to 'a companion'. Currently gaining experience of the web development world.</p>
+      <p className="mb-5">Hey Visitor! Thanks for stopping by. I'm Aryamann which translates to 'a companion'. I'm currently gaining experience in the web development world.</p>
       <p className="mb-5">I'm passionate about web and game development, animation illustration, and music. I like to push myself to take up new challenges and also solve complex puzzles.</p>
       
-      <p className="mb-2">Social Links</p>
-      <ul className="mb-5 underline">
-        <li><a href="https://github.com/acloseknityarn">Github</a></li>
-        <li><a href="https://www.linkedin.com/in/aryamann-verma-614189169/">LinkedIn</a></li>
-        <li><a href="https://twitter.com/acloseknityarn">Twitter</a></li>
-        <li><a href="https://www.instagram.com/acloseknityarn/">Instagram</a></li>  
-      </ul>
+      <h2 className="mb-4 text-2xl">You can find me on</h2>
+      
+      <SocialLinks />
+      
+      <p className="mb-5">I am pursuing Computer Science at Georgian College, Barrie. I am always prepared to meet new people, build connections and learn new things. 
+      I consider making a difference in the world through my work.</p>
 
-      <p className="mb-5">About Myself - I'm pursuing Computer Science at Georgian College, Barrie. I am always prepared to meet new people, build connections and learn new things. I consider making an incentive for individuals and affecting the world through my work.</p>
-      <p className="mb-5">Skills - programming languages</p> 
+      <h2 className="mb-5 text-2xl">Skills</h2>
 
       <p className="mb-5">Art Work - showcase</p>
       
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import SocialLinks from '../components/SocialLinks';
+import profilePic from '../public/images/portfolio-pic.jpg'
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-4xl mx-auto px-6">
+      {/* will talk about this later */}
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>Home Page</div>
-
       <div>
-        <img src="../img/portfolio-pic.jpg" alt="profile-pic" width="42" height="42"/>
+        {/* fullName */}
+        <div>
+            Aryamann Verma
+        </div>
+        
+        {/* profilePic */}
+        <Image
+          src={profilePic}
+          alt="Aryamann's Photograph"
+          priority
+        />
+        
+        <div className="mt-8 flex justify-center">
+          <SocialLinks />
+        </div>
       </div>
      
     </div>
