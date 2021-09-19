c0dfe53d6ab236f31508b8a7f9c3ddf3651a75cc
Aryamann Verma <avverma19699@gmail.com>
2021-09-19 15:03:45 -0400

    1. Create the Spotify currently playing track page. 2. Change the home-page logo. 3. Delete the Contact page. 4. Make the Project and Blog pages to be Coming Soon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import SocialLinks from '../components/SocialLinks'
@@ -11,35 +10,13 @@ import profilePic from '../public/images/portfolio-pic.jpg'
  */
 
 export default function Home() {
-  const [
-    spotifyCurrentlyPlayingData,
-    setSpotifyCurrentlyPlayingData
-  ] = useState(null)
-
-  /**
-   * executes whatever you put in it, only once
-   */
-  useEffect(() => {
-    // Promise syntax
-    fetch('/api/spotify-currently-playing')
-      .then(apiResponse => apiResponse.json()) // data comes from the server here
-      .then(fetchedData => { // this happens immediately
-        setSpotifyCurrentlyPlayingData(fetchedData)
-      })
-  }, [])
-
   return (
     <div className="max-w-4xl mx-auto px-6 mb-4">
       {/* will talk about this later */}
       <Head>
-        <title>Create Next App</title>
+        <title>Aryamann Verma</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div>
-        {spotifyCurrentlyPlayingData?.item?.name}
-      </div>
-
 
       <div className="md:w-3/5 md:mx-auto">
       <section className="mb-8">
